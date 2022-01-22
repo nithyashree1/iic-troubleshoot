7b5e2f7eb92550ec9568ff7480cb663c0155df01
nithyashree1 <82073854+nithyashree1@users.noreply.github.com>
2022-01-23 00:37:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const multer = require('multer');
 const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 const Comment = require("./models/comment");
@@ -14,14 +15,33 @@ mongoose.connect("mongodb://localhost:27017/login-app-db", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+// mongoose.connect('mongodb+srv://nithya-user:nithya-user@cluster0.r2cay.mongodb.net/iic-event2?retryWrites=true&w=majority',{ useNewUrlParser: true,useUnifiedTopology: true});
+
 
 app.use(express.static(__dirname + "./public/"));
 app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
 app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+var Storage = multer.diskStorage({
+  destination: function (request, file, callback) {
+      callback(null, "./public/uploads/")
+  },
+  filename: function (request, file, callback) {
+      // callback(null, Date.now() + file.originalname);
+      callback(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname));
+
+  }
+});
+
+
+var upload = multer({
+  storage: Storage
+});
 
 app.get("/", async (req, res) => {
   res.render("comments/home");
@@ -44,19 +64,14 @@ app.get("/event/:id/edit", async (req, res) => {
   const solution = await Comment.findById(id);
   res.render("comments/edit", { solution });
 });
-app.put("/event/:id", async (req, res) => {
+app.put("/event/:id", upload.single('file'),async (req, res) => {
   const { id } = req.params;
   if (!mongoose.Types.ObjectId.isValid(id)) return false;
 
   let newSolution = {
-    uname: req.body.uname,
-    email: req.body.email,
-    branch: req.body.branch,
-    semester: req.body.semester,
-    USN: req.body.usn,
-    wrongSol: req.body.wrongSol,
-    solution: req.body.solution,
-  };
+    stu_file:req.file.filename
+    
+  }
   const solution = await Comment.findByIdAndUpdate(id, newSolution, {
     runValidators: true,
     new: true,
@@ -72,15 +87,10 @@ app.get("/register", async (req, res) => {
   res.render("comments/register");
 });
 
-app.post("/", async (req, res) => {
+app.post("/",upload.single('file'), async (req, res) => {
   let newSolution = new Comment({
-    uname: req.body.uname,
-    email: req.body.email,
-    branch: req.body.branch,
-    semester: req.body.semester,
-    USN: req.body.usn,
-    wrongSol: req.body.wrongSol,
-    solution: req.body.solution,
+    stu_file:req.file.filename
+    
   });
   await newSolution.save();
   res.redirect(`/event/${newSolution._id}`);
