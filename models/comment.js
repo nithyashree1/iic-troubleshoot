67498b1e54215e1fbd428f30f8a5720fb5ee193e
nithyashree1 <82073854+nithyashree1@users.noreply.github.com>
2022-01-23 15:05:31 +0530

    Update comment.js

--- models/comment.js
+++ models/comment.js
@@ -2,11 +2,31 @@ const mongoose = require('mongoose');
 const Schema=mongoose.Schema
 
 const commentSchema=new Schema({
-   
+    uname:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    branch:{
+        type:String,
+        required:true
+    },
+    semester:{
+        type:String,
+        required:true
+    },
+    USN:{
+        type:String,
+        required:true
+    },
     stu_file:{
         type:String,
         required:true
     }
+  
  
 },{timestamps:true})
 
